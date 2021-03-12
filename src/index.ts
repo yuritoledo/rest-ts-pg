fb6fffbcf8e250bd7acbb206022984eb64d1ae93
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-11 21:07:22 -0300

    chore: adding validator and logger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, User } from "@prisma/client"
+import express from "express"
+import morgan from "morgan"
+import * as yup from 'yup'
+
+const app = express()
 
 const prisma = new PrismaClient()
 
-const main = async () => {
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/users', async (_, res) => {
   const users = await prisma.user.findMany()
+  res.json(users)
+})
 
-  console.log(users)
-}
+const userSchema = yup.object({
+  id: yup.string().notRequired(),
+  email: yup.string().required('email é obrigatório'),
+  name: yup.string().required('nome é obrigatório'),
+})
 
-main()
-  .catch(err => console.log('error', err))
-  .finally(() => prisma.$disconnect())
+app.post<{}, {}, User>('/users', async (req, res) => {
+  try {
+    await userSchema.validate(req.body, { abortEarly: false })
+
+    const newUser = await prisma.user.create({
+      data: req.body
+    })
+
+    res.json({ newUser })
+  } catch (error) {
+    console.error(error)
+    res.send(error)
+  }
+})
+
+app.listen(3000)
